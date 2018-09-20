dbcdeb9b96267640f309e5755f355dd6da645678
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-20 16:24:11 +0100

    Created sub-components (task-list has task-item)

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-Vue.component('task', {
-    template: '<li><slot></slot></li>',
+Vue.component('task-list', {
+    template: `
+    <div>
+        <task-item v-for="task in tasks" :key="task.id">
+            <input :id="'task_' + task.id" type="checkbox" v-model="task.completed">
+            <label :for="'task_' + task.id">{{ task.description }}</label>
+        </task-item>
+    </div>
+    `,
 
     data() {
         return {
-            message: "Hello",
+            tasks: [
+                { id: 1, description: "Do something Mutley!", completed: true },
+                { id: 2, description: "Catch that pidgeon", completed: false },
+                { id: 3, description: "Penelope Pitstop", completed: true },
+                { id: 4, description: "Dick Dastardly", completed: false },
+            ]
         }
     }
 });
 
+
+Vue.component('task-item', {
+    template: '<li class="unstyled-list"><slot></slot></li>',
+});
+
+
 new Vue({
     el: '#root',
-    
 });
