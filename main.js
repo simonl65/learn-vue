6d11369fdd83d1b38ae2f023a67ebe3b6b0e2235
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-21 16:00:33 +0100

    Clean-up for: 14 Named Slots in a Nutshell.

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-window.Event = new Vue();
-
-Vue.component('cd-coupon', {
-    methods: {
-        onBlur() {
-            // Make AJAX call, etc, then emit the 'applied' event...
-            Event.$emit('applied');
-        }
-    },
-
-    template: '<input placeholder="Enter coupon code" @blur="onBlur">',
-});
-
-
 new Vue({
     el: '#root',
-
-    data: {
-        couponWasApplied: false,
-    },
-
-    created() {
-        Event.$on( 'applied', () => console.log('I got it!') );
-    }
 });
