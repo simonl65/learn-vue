150a9a8fca293524ed8d3df69ae55ad7f3e8a565
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-21 11:44:45 +0100

    Cleared for new exercise.

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
 Vue.component('cd-modal', {
     template: `
-        <div class="modal is-active">
-            <div class="modal-background" @click="$emit('close-modal')"></div>
-            <div class="modal-content">
-                <div class="box">
-                    <slot></slot>
-                </div>
-            </div>
-
-            <button class="modal-close is-large" @click="$emit('close-modal')" aria-label="close"></button>
-        </div>
     `
 });
 
 
 new Vue({
     el: '#root',
-
-    data: {
-        showModal: false
-    },
-
-
 });
