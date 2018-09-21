775f3f927fc55a15f3330d3c16479315fc7ea5e3
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-21 15:14:28 +0100

    Event is emitted on field blur.

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component('cd-coupon', {
+    methods: {
+        onCouponAppliedLOCAL() {
+            // Make AJAX call, etc, then emit the 'applied' event...
+            this.$emit('applied', { data: 'VALID COUPON' });
+        }
+    },
+
+    template: '<input placeholder="Enter coupon code" @blur="onCouponAppliedLOCAL">',
+});
+
+
 new Vue({
     el: '#root',
+
+    methods: {
+        onCouponApplied(){
+            alert('Coupon was applied!');
+        }
+    }
 });
