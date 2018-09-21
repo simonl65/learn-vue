07f46bbbda3e7d6caa7daee2d667e0649bf7a7f2
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-21 13:00:59 +0100

    Tabs are working, but back button not showing correct tab.

--- main.js
+++ main.js
@@ -1,8 +1,67 @@
-Vue.component('cd-modal', {
+Vue.component('cd-tabs', {
+    data() {
+        return { tabs: [] };
+    },
+
+    created() {
+        this.tabs = (this.$children);
+    },
+
+    methods: {
+        selectTab( selectedTab ) {
+            this.tabs.forEach( tab => {
+                tab.isActive = ( tab.name == selectedTab.name )
+            });
+        }
+    },
+
     template: `
-    `
+    <div>
+        <div class="tabs">
+            <ul>
+                <li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive }">
+                    <a :href="tab.href" @click="selectTab( tab )">{{ tab.name }}</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="tabs-detail">
+            <slot></slot>
+        </div>
+    </div>
+    `,
 });
 
+
+Vue.component('tab', {
+    props: {
+        name: { required: true, type: String },
+        selected: { default: false, type: Boolean }
+    },
+
+    data() {
+        return {
+            isActive: false,
+        }
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+        this.isActive = this.selected;
+    },
+
+    template: `
+    <div v-show="isActive">
+        <slot></slot>
+    </div>
+    `,
+
+});
 
 new Vue({
     el: '#root',
